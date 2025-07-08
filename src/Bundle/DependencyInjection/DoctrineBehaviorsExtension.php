26c30ac8795c366ca610409229735990e39690cd
Ernesto Liberio <elv@teamq.biz>
2025-07-08 00:43:38 -0500

    Downgrades PHP version to 8.0
    The project is being downgraded to PHP 8.0 in the CI workflows to maintain compatibility.
    
    The workflows affected are:
    - code_checks.yaml
    - linter.yaml
    - tests.yaml

--- src/Bundle/DependencyInjection/DoctrineBehaviorsExtension.php
+++ src/Bundle/DependencyInjection/DoctrineBehaviorsExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TeamQ\DoctrineBehaviors\Bundle\DependencyInjection;
 
+use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -13,9 +14,9 @@ final class DoctrineBehaviorsExtension extends Extension
 {
     /**
      * @param array<array> $configs
-     * @throws \Exception
+     * @throws Exception
      */
-    public function load(array $configs, ContainerBuilder $container):void
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $phpFileLoader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../../../config'));
         $phpFileLoader->load('services.php');
